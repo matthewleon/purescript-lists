8e9ed61bf5d1a31f6cfa21137d78bed8f93f3a7a
Matthew Leon <ml@matthewleon.com>
2017-04-07 15:28:33 +0000

    test traverse_

--- test/Test/Main.purs
+++ test/Test/Main.purs
@@ -6,10 +6,7 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE)
 
 import Test.Assert (ASSERT)
-import Test.Data.List (testList)
 import Test.Data.List.Lazy (testListLazy)
-import Test.Data.List.Partial (testListPartial)
-import Test.Data.List.ZipList (testZipList)
 
 main :: forall eff. Eff (assert :: ASSERT, console :: CONSOLE | eff) Unit
 main = testListLazy
